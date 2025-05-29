85ee980a69809c228ee12826fe3f70d47e2627fc
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2025-05-29 15:34:24 +0700

    feat: update build script to handle environment configuration and improve image management

--- build.sh
+++ build.sh
@@ -1,10 +1,37 @@
 #!/bin/bash
-stty -echo
-echo "Enter your bitbucket password: "
-read PASSWORD
-stty echo
-git pull https://flukkyz:$PASSWORD@bitbucket.org/flukkyz/asean_talent.git
-if [ $? -eq 0 ]; then
+ENV=${1:-"test"}
+ACTION=${2:-"pull"}
+if [ $ENV = "test" ]; then
+  rm .env
+  cp .env.test .env
+  echo "Use Test ENV"
+else
+  rm .env
+  cp .env.prod .env
+  echo "Use Production ENV"
+fi
+if [ $ACTION = "pull" ]; then
+  stty -echo
+  echo "Enter your bitbucket password: "
+  read PASSWORD
+  stty echo
+  git pull https://flukkyz:$PASSWORD@bitbucket.org/flukkyz/asean_talent.git
+  if [ $? -eq 0 ]; then
+    IMAGE_ID=$(docker image ls --filter=reference="flukkyz/nuxt:asean" --format "{{.ID}}")
+    if [ -z "$IMAGE_ID" ]; then
+      echo "Old image not found. Start building..."
+    else
+      echo "Old image exists. Removing..."
+      docker rmi $IMAGE_ID
+      echo "Remove old images successful. Start building..."
+    fi
+    docker build -t flukkyz/nuxt:asean .
+    docker push flukkyz/nuxt:asean
+    echo "Docker build and push completed."
+  else
+      echo "Git pull failed"
+  fi
+else
   IMAGE_ID=$(docker image ls --filter=reference="flukkyz/nuxt:asean" --format "{{.ID}}")
   if [ -z "$IMAGE_ID" ]; then
     echo "Old image not found. Start building..."
@@ -16,6 +43,4 @@ if [ $? -eq 0 ]; then
   docker build -t flukkyz/nuxt:asean .
   docker push flukkyz/nuxt:asean
   echo "Docker build and push completed."
-else
-    echo "Git pull failed"
 fi
