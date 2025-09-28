a793cc4cc31279317df6709b2584b22e8861c657
KUNAKORN KATASAI <kunakorn.k@cmu.ac.th>
2025-09-28 10:15:26 +0700

    style: standardize quotes and formatting in seeder file for consistency

--- api/seeders/20201006031903-generate-seeder.js
+++ api/seeders/20201006031903-generate-seeder.js
@@ -1,18 +1,18 @@
-"use strict";
-const db = require("../models");
-const setting = require("./generates/setting");
+'use strict'
+const db = require('../models')
+const setting = require('./generates/setting')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert("setting", setting);
+    await queryInterface.bulkInsert('setting', setting)
     await db.User.create({
-      name: "Admin",
-      email: "admin@admin.admin",
-      password: "Ss123456",
-      role: "admin",
-    });
+      name: 'Admin',
+      email: 'admin@admin.admin',
+      password: 'Ss123456',
+      role: 'admin'
+    })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("setting", null);
-    await queryInterface.bulkDelete("users", null);
-  },
-};
+    await queryInterface.bulkDelete('setting', null)
+    await queryInterface.bulkDelete('users', null)
+  }
+}
