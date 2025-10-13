f81c39f8ccb60b9622f1d1496b58f825f3037fa8
flukkyz555 <kunakorn.flukky@gmail.com>
2025-10-13 23:51:10 +0700

    feat: add model-detail description to domain industries component

--- pages/backend/settings/references/domain-industries.vue
+++ pages/backend/settings/references/domain-industries.vue
@@ -1,5 +1,9 @@
 <template>
-  <lists-name api="domain-industries" model-name="Domain Industry" disabled-delete />
+  <lists-name
+    api="domain-industries"
+    model-name="Domain Industry"
+    disabled-delete model-detail="Refers to the specific sector or category of economic activity that a business or organization operates within. It helps classify the nature of the business for analysis, reporting, and regulatory purposes. Examples include BCG, Earth and Space, and AI."
+  />
 </template>
 
 <script>
